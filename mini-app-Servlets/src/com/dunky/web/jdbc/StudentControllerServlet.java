76dbef1bd54bcf142d815572df1effc51db3ccc8
dunky-star <dukygeoffrey39@gmail.com>
2022-09-26 17:10:24 +0300

    Add functionality to perfom search

--- mini-app-Servlets/src/com/dunky/web/jdbc/StudentControllerServlet.java
+++ mini-app-Servlets/src/com/dunky/web/jdbc/StudentControllerServlet.java
@@ -69,6 +69,10 @@ public class StudentControllerServlet extends HttpServlet {
 			case "DELETE":
 				deleteStudent(request, response);
 				break;
+				
+			case "SEARCH":
+                searchStudents(request, response);
+                break;
 								
 			default:
 				listStudents(request, response);
@@ -82,6 +86,22 @@ public class StudentControllerServlet extends HttpServlet {
 	}
 
 	
+    private void searchStudents(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // read search name from form data
+        String theSearchName = request.getParameter("theSearchName");
+        
+        // search students from db util
+        List<Student> students = studentDbUtil.searchStudents(theSearchName);
+        
+        // add students to the request
+        request.setAttribute("STUDENT_LIST", students);
+                
+        // send to JSP page (view)
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/list-students.jsp");
+        dispatcher.forward(request, response);
+    }
+
+
 	private void deleteStudent(HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 
